dec0a526be792abfea0c9130711f2e50439ee760
YJ Soon <yjsoon@tinkertanker.com>
2025-05-13 19:25:54 +0800

    Initial commit: Set up password-protected URL shortener with short.io API

---
+++ env.js
@@ -0,0 +1,7 @@
+// This file will be generated during build time with the actual environment variables
+window.env = {
+  // Default values (will be overwritten during build)
+  PASSWORD: 'demo',
+  SHORT_IO_API_KEY: '',
+  SHORT_DOMAIN: ''
+};
